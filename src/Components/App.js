57df51affff2f87ffc2e0f5a1429243a3d4c77cd
awpala <adam.w.pala@gmail.com>
2020-09-18 17:50:54 -0500

    Remove create-react-app boilerplate

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        {"Text goes here"}
+      </header>
+    </div>
+  );
+}
+
+export default App;
